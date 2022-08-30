39ef946cec2735fb245fd392b658cf0ae4d20120
soojin <2425sja>
2022-08-30 14:33:57 +0900

    ck/src/services/awardService.js

---
+++ back/src/db/schemas/award.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const AwardSchema = new Schema(
+  {
+    award_id: {
+      type: String,
+      required: true,
+    },
+    award: {
+      type: String,
+      required: true,
+    },
+    detail: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const AwardModel = model("Education", AwardSchema);
+
+export { AwardModel };
