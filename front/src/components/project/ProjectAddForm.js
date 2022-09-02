ce1f76ad39ee988fe4286813b0e3562a52894fc9
선희 김 <ssunnykku@gmail.com>
2022-09-02 22:20:52 +0900

    프로젝트 버튼 설정 수정

--- front/src/components/project/ProjectAddForm.js
+++ front/src/components/project/ProjectAddForm.js
@@ -22,14 +22,14 @@ function ProjectAddForm({ setProjects, setIsEditing, portfolioOwnerId }) {
       });
 
     };
-
         // 필수값 입력 확인
         const isTitleValid = title.length > 0;
         const isFromValid = from.length > 0;
         const isToValid = to.length > 0;
+        const isDuration = from < to;
     
         //필수값 조건 동시에 만족되는지 확인
-        const isFormValid = isTitleValid && isFromValid && isToValid;
+        const isFormValid = isTitleValid && isFromValid && isToValid && isDuration;
 
         // 날짜 입력 조건
         const isDateValid = isFromValid && isToValid;
@@ -45,7 +45,9 @@ function ProjectAddForm({ setProjects, setIsEditing, portfolioOwnerId }) {
           } else if (from > to) {
             return  ( <Form.Text style={{color: 'tomato', fontWeight: 'bolder' }}>
                   해당 기간의 조회가 불가능합니다.
-                  </Form.Text>)
+                  </Form.Text>
+                  
+                  )
           }
         }
 
